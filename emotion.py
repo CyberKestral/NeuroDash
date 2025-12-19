48be28c2061fcf1c181106fc7ced561dd9c918bd
CyberKestral <kbyte79@gmail.com>
2025-12-19 13:03:46 +0530

    Update emotion.py

--- emotion.py
+++ emotion.py
@@ -1,1 +1,12 @@
 
+from textblob import TextBlob
+
+def detect_emotion(text):
+    polarity = TextBlob(text).sentiment.polarity
+
+    if polarity > 0.3:
+        return "positive"
+    elif polarity < -0.3:
+        return "negative"
+    else:
+        return "neutral"
